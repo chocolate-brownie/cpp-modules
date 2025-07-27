3579a9de668b0b785c9fa604dbb21e133485e16e
Milan Godawatta <mgodawat@student.42.fr>
2025-07-27 21:28:34 +0200

    update

---
+++ module-00/Lessons/src/constructor_overloading.cpp
@@ -0,0 +1,31 @@
+/*
+#include <iostream>
+
+using namespace std;
+
+class Computer {
+public:
+  string brand;
+  string model;
+
+  Computer() {
+    brand = "Unknown";
+    model = "Unknown";
+  }
+
+  Computer(string x, string y) {
+    brand = x;
+    model = y;
+  }
+};
+
+int main(void) {
+  Computer laptop1;
+  Computer laptop2("HP", "Omen");
+  Computer laptop3("Dell", "XPS");
+  cout << "Your computer is " << laptop1.brand << ": " << laptop1.model << endl;
+  cout << "Your computer is " << laptop2.brand << ": " << laptop2.model << endl;
+  cout << "Your computer is " << laptop3.brand << ": " << laptop3.model << endl;
+  return 0;
+}
+ */
