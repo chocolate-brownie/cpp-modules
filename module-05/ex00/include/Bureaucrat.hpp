f0cf3c6de73e8fd335c7cd4e3436ef88ae29542e
Milan Godawatta <milanoshae@gmail.com>
2025-09-24 21:36:23 +0200

    update

--- module-05/ex00/include/Bureaucrat.hpp
+++ module-05/ex00/include/Bureaucrat.hpp
@@ -10,19 +10,28 @@ class Bureaucrat {
     unsigned int _grade;
 
   public:
-    Bureaucrat(const std::string name, unsigned int grade);
+    Bureaucrat();
     Bureaucrat(const Bureaucrat& other);
     Bureaucrat& operator=(const Bureaucrat& other);
     ~Bureaucrat();
 
-    std::string getName();
-    unsigned int getGrade();
+    Bureaucrat(const std::string name, unsigned int grade);
 
-    class GradeTooHighException : public std::exception {};
-    class GradeTooLowException : public std::exception {};
+    std::string getName() const;
+    unsigned int getGrade() const;
 
     void incrementGrade();
     void decrementGrade();
+
+    class GradeTooHighException : public std::exception {
+      public:
+        virtual const char* what() const throw();
+    };
+
+    class GradeTooLowException : public std::exception {
+      public:
+        virtual const char* what() const throw();
+    };
 };
 
 std::ostream& operator<<(std::ostream& os, const Bureaucrat& obj);
--- module-05/ex00/src/Bureaucrat.cpp
+++ module-05/ex00/src/Bureaucrat.cpp
@@ -1,3 +1,58 @@
 #include "../include/Bureaucrat.hpp"
+#include <iostream>
+
+/* -------------------- Orthodox Canonical Class Form ------------------ */
+Bureaucrat::Bureaucrat() : _name(""), _grade(150) {
+    std::cout << "Default constructor called" << std::endl;
+    return;
+}
+
+Bureaucrat::Bureaucrat(const Bureaucrat& other)
+    : _name(other._name), _grade(other._grade) {
+    std::cout << "Copy constructor called for " << _name << std::endl;
+}
+
+Bureaucrat& Bureaucrat::operator=(const Bureaucrat& other) {
+    if (this != &other) {
+        std::cout << "Assignment operator called for " << _name << std::endl;
+    }
+    _grade = other._grade;
+    return *this;
+}
+
+Bureaucrat::~Bureaucrat() {
+    std::cout << "Destructor called for " << _name << std::endl;
+}
+
+/* -------------------- Constructor with Parameter ------------------ */
 Bureaucrat::Bureaucrat(const std::string name, unsigned int grade)
-    : _name(name) {}
+    : _name(name), _grade(grade) {
+    std::cout << "Parameter constructor called" << std::endl;
+    if (grade < 1) throw GradeTooHighException();
+    if (grade > 150) throw GradeTooLowException();
+    return;
+}
+
+/* -------------------- Exception throws ------------------ */
+const char* Bureaucrat::GradeTooHighException::what() const throw() {
+    return "Grade is too high (must be >= 1)";
+}
+
+const char* Bureaucrat::GradeTooLowException::what() const throw() {
+    return "Grade is too low (must be <= 150)";
+}
+
+/* -------------------- Other member functions ------------------ */
+void Bureaucrat::incrementGrade() {
+    if (_grade <= 1) throw GradeTooHighException();
+    _grade--;
+}
+
+void Bureaucrat::decrementGrade() {
+    if (_grade >= 150) throw GradeTooLowException();
+    _grade++;
+}
+
+/* -------------------- Getters & Setters ------------------ */
+std::string Bureaucrat::getName() const { return _name; }
+unsigned int Bureaucrat::getGrade() const { return _grade; }
