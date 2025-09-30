eea5e36a2f600b0ad191e151a6394fd801976f25
Milan Godawatta <milanoshae@gmail.com>
2025-10-01 01:40:38 +0200

    feat: Add ShrubberyCreationForm and RobotomyRequestForm
    Added the implementation for ShrubberyCreationForm and
    RobotomyRequestForm and added basic tests.

--- module-05/ex02/src/main.cpp
+++ module-05/ex02/src/main.cpp
@@ -1,4 +1,5 @@
 #include "../include/AForm.hpp"
+#include "../include/Colors.hpp"
 #include "../include/RobotomyRequestForm.hpp"
 #include "../include/ShrubberyCreationForm.hpp"
 #include <cstdlib>
@@ -8,13 +9,30 @@
 int main() {
     std::srand(std::time(0));
 
-    ShrubberyCreationForm shrubForm("Graden");
+    ShrubberyCreationForm shrubForm("Graden"); // Sign: 145, Exec: 137
     std::cout << shrubForm << std::endl;
 
-    RobotomyRequestForm robReqForm("Optimus Prime");
+    RobotomyRequestForm robReqForm("Optimus Prime"); // Sign: 72, Exec 45
     std::cout << robReqForm << std::endl;
-    
-    
+
+    Bureaucrat highGrade("Alice(highGrade)", 1);
+    Bureaucrat midGrade("Charlie(midGrade)", 67);
+    Bureaucrat lowGrade("Shelock(lowGrade)", 144);
+
+    std::cout << BOLD_WHITE << "----------------------" << RESET << std::endl;
+    std::cout << GREEN << "-- Execute without signing --" << RESET << std::endl;
+    highGrade.executeForm(shrubForm);
+    highGrade.signForm(shrubForm);
+    highGrade.executeForm(shrubForm);
+    std::cout << BOLD_WHITE << "----------------------" << RESET << std::endl;
+
+    std::cout << GREEN << "Sign but execute with low grade" << RESET
+              << std::endl;
+    midGrade.signForm(robReqForm);    // can sign
+    midGrade.executeForm(robReqForm); // but cannot execute
+    highGrade.executeForm(robReqForm);
+    highGrade.signForm(robReqForm);
+    highGrade.executeForm(robReqForm);
 
     return 0;
 }
