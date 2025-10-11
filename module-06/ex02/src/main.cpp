34d46d39e6c228c4339fa0fb9df4bd3a4a74f55e
Milan Godawatta <milanoshae@gmail.com>
2025-10-11 18:48:11 +0200

    feat: Implement dynamic cast for Base class
    Adds Base.cpp and updates main.cpp to use dynamic_cast for identifying
    the actual type of a Base object. Includes error handling for reference
    casts.

--- module-06/ex02/src/main.cpp
+++ module-06/ex02/src/main.cpp
@@ -3,8 +3,9 @@
 #include "../include/Base.hpp"
 #include "../include/C.hpp"
 #include <cstdlib>
+#include <ctime>
+#include <exception>
 #include <iostream>
-#include <vector>
 
 /* dynamic_cast is a C++ operator used for safely converting pointers and
  * references within an inheritance hierarchy, relying on Runtime Type
@@ -25,25 +26,61 @@
 
  */
 Base* generate(void) {
-    std::vector<Base*> vec = {new A(), new B(), new C()};
-    int idx = std::rand() % vec.size();
-    return vec[idx];
+    int choice = std::rand() % 3;
+    switch (choice) {
+    case 0:
+        return new A();
+    case 1:
+        return new B();
+    default:
+        return new C();
+    }
 }
 
+/* if you can cast p to an A*, B* or *C print the message otherwise give me
+ a nullptr */
 void identify(Base* p) {
     if (A* a = dynamic_cast<A*>(p)) {
+        (void)a;
         std::cout << "A" << std::endl;
     } else if (B* b = dynamic_cast<B*>(p)) {
+        (void)b;
         std::cout << "B" << std::endl;
     } else if (C* c = dynamic_cast<C*>(p)) {
+        (void)c;
         std::cout << "C" << std::endl;
     }
 }
 
-void identify(Base& p) {}
+/* "Is this Base object actually an A object?" The way it answers "no" has to be
+ different for pointers and references. Because a null reference is
+ impossible, dynamic_cast needs a different way to signal a failure. The
+ language designers chose to use the C++ exception system. If the cast fails, it
+ throws a std::bad_cast exception. */
+void identify(Base& p) {
+    try {
+        A& a = dynamic_cast<A&>(p);
+        (void)a;
+        std::cout << "A" << std::endl;
+    } catch (const std::exception& e) {
+        try {
+            B& b = dynamic_cast<B&>(p);
+            (void)b;
+            std::cout << "B" << std::endl;
+        } catch (const std::exception& e) { std::cout << "C" << std::endl; }
+    }
+}
 
 int main(void) {
+    std::srand(std::time(0));
+
     Base* randomObj = generate();
-    std::cout << "Identifying with pointer" << std::endl;
+    std::cout << "Identifying with pointer: ";
     identify(randomObj);
+
+    std::cout << "Identifying with reference: ";
+    identify(*randomObj);
+
+    delete randomObj;
+    return 0;
 }
