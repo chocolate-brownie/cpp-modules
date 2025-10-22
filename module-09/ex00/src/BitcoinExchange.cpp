c27b8e8451d86f564f4c0450e8596cb6009ef3bd
mgodawat <mgodawat@student.42.fr>
2025-10-22 19:51:24 +0200

    fixing module-09 ocf methods issues and adding ref for examrank05

--- module-09/ex00/src/BitcoinExchange.cpp
+++ module-09/ex00/src/BitcoinExchange.cpp
@@ -5,7 +5,7 @@ BitCoinExchange::BitCoinExchange() : _dbFilePath("data/data.csv") {
     BitCoinExchange::_parseDbFile(_dbFilePath);
 
 #ifdef DEBUG
-    std::cout << "\n[DEBUG]: Database parsing complete. Map size is: "
+    std::cout << "\n[DEBUG]: Constructor. Map size is: "
               << _priceDatabase.size() << " ---" << std::endl;
     std::map<std::string, double>::iterator it = _priceDatabase.begin();
     for (int i = 0; i < 5 && it != _priceDatabase.end(); ++i, ++it) {
@@ -13,6 +13,26 @@ BitCoinExchange::BitCoinExchange() : _dbFilePath("data/data.csv") {
     }
     std::cout << std::endl;
 #endif
+}
+
+BitCoinExchange::BitCoinExchange(const BitCoinExchange& other)
+    : _dbFilePath(other._dbFilePath) {
+    _priceDatabase = other._priceDatabase;
+    _priceInputFile = other._priceInputFile;
+#ifdef DEBUG
+    std::cout << "[DEBUG]: Copy constructor called" << std::endl;
+#endif
+}
+
+BitCoinExchange& BitCoinExchange::operator=(const BitCoinExchange& other) {
+#ifdef DEBUG
+    std::cout << "[DEBUG]: Copy assignment operator called" << std::endl;
+#endif
+    if (this != &other) {
+        _priceDatabase = other._priceDatabase;
+        _priceInputFile = other._priceInputFile;
+    }
+    return *this;
 }
 
 // -------------------- Deconstructor ------------------------------------
